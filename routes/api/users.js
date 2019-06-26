7ce135078c21258f0eb00b547f7f5e3567f60c35
Jing <jingkcheng@hotmail.com>
2019-06-26 19:09:08 -0400

    User registration completed with JWT

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator/check');
+const { check, validationResult } = require('express-validator');
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 const User = require('../../models/User');
 
@@ -63,10 +65,21 @@ router.post(
     
             await user.save();
     
-            //Return jsonwebtoken
-    
-            res.send('User registered'); 
-
+            //Return jsonwebtoken - Encryption when sending JSON information through the web
+            /* https://jwt.io/introduction/ */
+            const payload = {
+                user: {
+                    id: user.id 
+                }
+            }
+            jwt.sign(
+                payload,
+                config.get('jwtSecret'),
+                { expiresIn: 360000 }, 
+                (err, token) => {
+                if(err) throw err;
+                res.json({ msg: 'Registration Successful!', token });
+            });
         }
     }
     catch(err){
